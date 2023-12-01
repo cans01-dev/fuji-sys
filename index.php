44b70f0b37db585d883691b71393889daa7b50c4
cans01-dev <web@cans01.jp>
2023-12-01 15:42:27 +0900

    first-commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php 
+require 'vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', 'index');
+
+    // {id} must be a number (\d+)
+    // $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+    // The /{title} suffix is optional
+    // $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+function index() {
+    require_once 'pages/top.php';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700;900&family=Poppins:wght@400;600&display=swap" rel="stylesheet"></head>
+    <!-- css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- scripts -->
+    
+<body>
+    <?php 
+    require 'templates/header.php';
+
+    switch ($routeInfo[0]) {
+        case FastRoute\Dispatcher::NOT_FOUND:
+            // ... 404 Not Found
+            break;
+        case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            $allowedMethods = $routeInfo[1];
+            // ... 405 Method Not Allowed
+            break;
+        case FastRoute\Dispatcher::FOUND:
+            $handler = $routeInfo[1];
+            $vars = $routeInfo[2];
+            if (!empty($vars)) {
+                // ... call $handler with $vars
+                echo $handler($vars);
+            } else {
+                echo $handler();
+            }
+    
+            break;
+    }    
+    ?>
+
+</body>
+</html>
